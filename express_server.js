8da02f4ea37657c076700be4b3907b2227703ff2
Dan Xue <danixue@gmail.com>
2021-09-30 16:57:14 +0000

    tinyapp with cookie

--- express_server.js
+++ express_server.js
@@ -1,4 +1,6 @@
 const express = require("express")
+const cookieParser = require("cookie-parser")
+
 const app  = express()
 const PORT = 8080;
 
@@ -6,6 +8,8 @@ app.set("view engine", "ejs");
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(cookieParser())
 
 function generateRandomString() {
   
@@ -19,11 +23,6 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
 app.post("/urls", (req, res) => {
   // console.log(req.body);  // Log the POST request body to the console
   let tempShortURL = generateRandomString();
@@ -32,12 +31,33 @@ app.post("/urls", (req, res) => {
   // console.log(urlDatabase);
 });
 
+app.post("/login", (req, res) => {
+  let username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+})
+
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");         // Respond with 'Ok' (we will replace this)
   // console.log(urlDatabase);
 });
 
+app.post("/urls/:shortURL", (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL
+  res.redirect("/urls");
+})
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect("/urls");
+})
+
+//this is the main page
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  res.render("urls_index", templateVars);
+});
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
